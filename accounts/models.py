c8d4e395741245b5e256b92feaefd2ae8b483de5
Lourence <ma6957692@gmail.com>
2026-07-03 15:22:02 +0800

    check point

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class Users(AbstractUser):
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+
+    ROLE_COICES = [
+        ('admin', 'Admin'),
+        ('staff', 'Staff')
+    ]
+
+    role = models.CharField(max_length=10, choices=ROLE_COICES, default='staff')
+
+    def __str__(self):
+        return self.username
--- inventory/urls.py
+++ inventory/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import views
+from . import views
 
 router = DefaultRouter()
 router.register(r'products', views.ProductViewSet, basename='product')
-router.register(r'ingredients', views.IngredientViewSet, basename='ingredient')
-router.register(r'product-batches', views.ProductBatchViewSet, basename='productbatch')
-router.register(r'ingredient-batches', views.IngredientBatchViewSet, basename='ingredientbatch')
+# TODO: Migrate VVV To their own .py files and register them here
+# router.register(r'ingredients', views.IngredientViewSet, basename='ingredient')
+# router.register(r'product-batches', views.ProductBatchViewSet, basename='productbatch')
+# router.register(r'ingredient-batches', views.IngredientBatchViewSet, basename='ingredientbatch')
 
 urlpatterns = [
   path('', include(router.urls))
